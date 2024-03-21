f52838fc1579547ba0d0c75c7d2f186902582b47
Ali <alidevecioglu12324@gmail.com>
2024-03-21 13:34:23 +0200

    Update

--- Models/Categories.cs
+++ Models/Categories.cs
@@ -1,8 +1,7 @@
 namespace WebHelloWorld.Models;
-public class Product
+public class Categories
 {
     public Guid Id { get; set; } = Guid.NewGuid();
     public required string Name { get; set; }
-    public decimal Price { get; set; }
-    public Categories? Category { get; set; } // Add this property
+    public string? Description { get; set; }
 }
