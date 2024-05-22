581dc929ffc3c859c73555871407299fb10e4dac
remainedmind <lemoonadresse@gmail.com>
2024-05-22 20:42:30 +0300

    Added .env file support

--- Program.cs
+++ Program.cs
@@ -1,12 +1,25 @@
 using Microsoft.EntityFrameworkCore;
 using WebHelloWorld.Data;
+using DotNetEnv;
 
 var builder = WebApplication.CreateBuilder(args);
 
+
+Env.Load(); // Loading from .env file
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+var connectionString = $"Host={Environment.GetEnvironmentVariable("DB_HOST")};" +
+                       $"Port={Environment.GetEnvironmentVariable("DB_PORT")};" +
+                       $"Database={Environment.GetEnvironmentVariable("DB_DATABASE")};" +
+                       $"Username={Environment.GetEnvironmentVariable("DB_USERNAME")};" +
+                       $"Password={Environment.GetEnvironmentVariable("DB_PASSWORD")}";
+
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+    options.UseNpgsql(
+        // builder.Configuration.GetConnectionString("DefaultConnection"))
+        connectionString)
+    );
 
 var app = builder.Build();
 
